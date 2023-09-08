a1977babfaa663ed806723f66310438dd26dbb26
Johanne ESSIERE <essierejohanne1@gmail.com>
2023-09-07 18:51:04 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,9 +4,10 @@ if __name__ == "__main__":
     arg_len = len(argv) - 1
     if arg_len == 0:
         print("{:d} arguments.".format(arg_len))
+    elif arg_len == 1:
+        print("{:d} argument:".format(arg_len))
     else:
-        comment = "{:d} argument:".format(arg_len) if arg_len == 1 else "{:d} arguments:".format(arg_len)
-        print(comment)
+        print("{:d} arguments:".format(arg_len))
         count = 0
         for arg in argv:
             if count != 0:
