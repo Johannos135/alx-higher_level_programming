325f33f23a6b9ea80e4bf16265388ac33d14cb1d
Johanne ESSIERE <essierejohanne1@gmail.com>
2023-09-07 19:02:27 -0700

    new commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
-    arg_len = len(argv) - 1
-    if arg_len == 0:
-        print("{:d} arguments.".format(arg_len))
-    elif arg_len == 1:
-        print("{:d} argument:".format(arg_len))
+    import sys
+    i = len(sys.argv) - 1
+
+    if i == 0:
+        print("{} arguments.".format(i))
+    elif i == 1:
+        print("{} argument:".format(i))
     else:
-        print("{:d} arguments:".format(arg_len))
-        count = 0
-        for arg in argv:
-            if count != 0:
-                print("{:d}: {}".format(count, arg))
-            count += 1
+        print("{} arguments:".format(i))
+
+    if i >= 1:
+        i = 0
+        for arg in sys.argv:
+            if i != 0:
+                print("{}: {}".format(i, arg))
+            i += 1
