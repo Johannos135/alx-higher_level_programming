5f426b32175c90c37a9dbda25fcc09aae44717a2
Johanne ESSIERE <essierejohanne1@gmail.com>
2023-09-11 14:28:29 -0700

    8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    '''
-        returns a tuple with the length of a string
-        and its first character.
-    '''
     len_sentence = len(sentence)
-    if len_sentence == 0:
-        new_tuple = (len_sentence, None)
-    else:
+    if not sentence:
         new_tuple = (len_sentence, sentence[0])
+    else:
+        new_tuple = (len_sentence, None)
 
     return (new_tuple)
-
