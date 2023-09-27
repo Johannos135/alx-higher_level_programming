3b4efbc6fba9b8a559e3c29987d9f6767eda82dc
Johanne ESSIERE <essierejohanne1@gmail.com>
2023-09-27 11:35:48 -0700

    new 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Define a Square class"""
 class Square:
+    """Defines class constructor"""
     def __init__(self, size):
+        """Defines our private variable"""
         self.__size = size
