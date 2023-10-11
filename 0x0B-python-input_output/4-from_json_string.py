97514248d42c17a04b4774f08bc0717993aefbe6
Johanne ESSIERE <essierejohanne1@gmail.com>
2023-10-10 18:07:04 -0700

    4-from_json_string.py

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Module 4-from_json_string """
+import json
+
+
+def from_json_string(my_str):
+    """Function that returns an object representation
+
+        Args:
+            my_str: json string
+
+        Returns:
+            Object represention by a json string
+    """
+    return json.load(my_str)
