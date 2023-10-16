f9c4ae08bad96f123bc4577c5246b55c12127862
Johanne ESSIERE <essierejohanne1@gmail.com>
2023-10-16 13:08:00 -0700

    Pycodestyle spec

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,12 +3,13 @@
 
 
 class Base:
-    """ Class base """
+    """ Class Base """
+
     __nb_objects = 0
 
     def __init__(self, id=None):
         """ Class Base COnstructor
-            
+
             Parameters:
                 id: Public instance of class
         """
