d48e70f4e68c8266151679c5b8bfccea470f8828
Johanne ESSIERE <essierejohanne1@gmail.com>
2023-12-11 15:12:47 -0800

    parsing

--- 8-square.js
+++ 8-square.js
@@ -5,9 +5,9 @@ const { argv } = require('process');
 if (isNaN(argv[2])) {
   console.log('Missing size');
 } else {
-  for (let i = 0, s; i < argv[2]; i++) {
+  for (let i = 0, s; i < parseInt(argv[2]); i++) {
     s = '';
-    for (let j = 0; j < argv[2]; j++) {
+    for (let j = 0; j < parseInt(argv[2]); j++) {
       s += 'X';
     }
     console.log(s);
